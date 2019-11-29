cce42cd398b530ae0025f811da442f67f01d3838
yj1399 <yashraj1399@gmail.com>
2019-11-29 23:39:30 +0530

    First Commit

---
+++ web/web.go
@@ -0,0 +1,30 @@
+package web
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/andyxning/shortme/conf"
+	"github.com/andyxning/shortme/web/api"
+	"github.com/andyxning/shortme/web/www"
+
+	"github.com/gorilla/mux"
+)
+
+func Start() {
+	log.Println("web starts")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/version", api.CheckVersion).Methods(http.MethodGet)
+	r.HandleFunc("/health", api.CheckHealth).Methods(http.MethodGet)
+	r.HandleFunc("/short", api.ShortURL).Methods(http.MethodPost).HeadersRegexp("Content-Type", "application/json")
+	r.HandleFunc("/expand", api.ExpandURL).Methods(http.MethodPost).HeadersRegexp("Content-Type", "application/json")
+	r.HandleFunc("/{shortenedURL:[a-zA-Z0-9]{1,11}}", api.Redirect).Methods(http.MethodGet)
+
+	r.HandleFunc("/index.html", www.Index).Methods(http.MethodGet)
+
+	r.Handle("/static/{type}/{file}", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	r.Handle("/favicon.ico", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+
+	log.Fatal(http.ListenAndServe(conf.Conf.Http.Listen, r))
+}
