30278d8a96078db49552df56e5620a3acbe55211
Yash Raj Singh <40159239+yj1399@users.noreply.github.com>
2019-12-02 01:18:45 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/andyxning/shortme/conf"
-	"github.com/andyxning/shortme/short"
-	"github.com/andyxning/shortme/web"
+	"github.com/yj1399/shortme/conf"
+	"github.com/yj1399/shortme/short"
+	"github.com/yj1399/shortme/web"
 )
 
 func main() {
